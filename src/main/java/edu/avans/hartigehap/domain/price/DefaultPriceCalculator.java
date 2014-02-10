1e268901edc4f476922fefb8bd2a8fa7254703f8
Gijs-W <gijsvandewater+github@gmail.com>
2014-02-10 12:27:31 +0100

    DefaultPriceCalculator

--- src/main/java/edu/avans/hartigehap/domain/price/DefaultPriceCalculator.java
+++ src/main/java/edu/avans/hartigehap/domain/price/DefaultPriceCalculator.java
@@ -1,7 +1,16 @@
 package edu.avans.hartigehap.domain.price;
 
+import java.util.List;
+
 /**
  * Created by Gijs on 10-2-14.
+ *
+ * Default price calculator.
  */
-public class DefaultPriceCalculator {
+public class DefaultPriceCalculator implements PriceCalculator {
+
+    @Override
+    public double calculatePrice(List<WebOrderItem> orderList) {
+        return 0;
+    }
 }
