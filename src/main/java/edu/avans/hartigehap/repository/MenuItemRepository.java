64e65654e229b914b89a6cfe7e46e3a477a5629f
Gijs-W <gijsvandewater+github@gmail.com>
2014-03-18 13:58:41 +0100

    Initial support for pagination
    Selected page hardcoded for now.

--- src/main/java/edu/avans/hartigehap/repository/MenuItemRepository.java
+++ src/main/java/edu/avans/hartigehap/repository/MenuItemRepository.java
@@ -1,6 +1,8 @@
 package edu.avans.hartigehap.repository;
 
 import edu.avans.hartigehap.domain.FoodCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import edu.avans.hartigehap.domain.MenuItem;
 
@@ -9,4 +11,6 @@ import java.util.List;
 public interface MenuItemRepository extends PagingAndSortingRepository<MenuItem, String> {
 
     List<MenuItem> findByFoodCategoriesId(long pizzaId);
+
+    Page<MenuItem> findByFoodCategoriesId(long pizzaId, Pageable page);
 }
--- src/main/java/edu/avans/hartigehap/service/jpa/MenuItemServiceImpl.java
+++ src/main/java/edu/avans/hartigehap/service/jpa/MenuItemServiceImpl.java
@@ -8,6 +8,9 @@ import edu.avans.hartigehap.service.MenuItemService;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -19,6 +22,8 @@ import java.util.List;
 @Repository
 public class MenuItemServiceImpl implements MenuItemService{
 
+    private static final int ITEMS_PER_PAGE = 3;
+
     @Autowired
     private MenuItemRepository menuItemRepository;
 
@@ -27,7 +32,32 @@ public class MenuItemServiceImpl implements MenuItemService{
         return menuItemRepository.findByFoodCategoriesId(pizzaId);
     }
 
-	@Override
+    /**
+     * Finds MenuItems with Pagination support
+     *
+     * @param pizzaId
+     * @param pageIndex
+     * @return MenuItems list
+     */
+    @Override
+    public Page<MenuItem> findByFoodCategoriesIdPaginated(long pizzaId, int pageIndex) {
+
+        Page<MenuItem> requestedPage = menuItemRepository.findByFoodCategoriesId(pizzaId, constructPageSpecification(pageIndex));
+
+        return requestedPage;
+    }
+
+    /**
+     * Constructs Pageable object
+     *
+     * @param pageIndex
+     * @return Pageable
+     */
+    private Pageable constructPageSpecification(int pageIndex) {
+        return new PageRequest(pageIndex, ITEMS_PER_PAGE);
+    }
+
+    @Override
 	public MenuItem findOne(String name) {
 		return menuItemRepository.findOne(name);
 	}
